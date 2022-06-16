89c35b78e6c794573eb61964fb1107d91aa6c119
Norberto <norbertoalves22@hotmail.com>
2022-06-16 23:28:31 +0200

    gamee

---
+++ arkanoid/game.py
@@ -0,0 +1,23 @@
+import pygame as pg
+
+from arkanoid import ANCHO, ALTO
+
+class Arkanoid:
+    def __init__(self):
+        print("Arranca el juego")
+        pg.init()
+        pg.display.set_mode((ANCHO, ALTO))
+
+    def jugar(self):
+        """Este es el bucle principal."""
+        salir = False
+        while not salir:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    salir = True
+            self.display.fill((99, 99, 99))
+            pg.display.flip()
+
+
+if __name__ == "__main__":
+    Arkanoid()
