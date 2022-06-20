a61580669863a89ca95c8b61efca38925dab7a51
Norberto <norbertoalves22@hotmail.com>
2022-06-20 20:35:31 +0200

    instalamos autopep8

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from arkanoid.game import Arkanoid
 
 if __name__ == "__main__":
     print(f"tamaño de la pantalla es ¨{ANCHO} x {ALTO}")
-    
-    Arkanoid()
+    juego = Arkanoid()
+    juego.juego()
