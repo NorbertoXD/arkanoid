2f3e27a5d1013750d200939152081ef0488f13b3
Norberto <norbertoalves22@hotmail.com>
2022-06-27 23:30:08 +0200

    AMANCE

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from arkanoid import ANCHO, ALTO
 from arkanoid.game import Arkanoid
 
 if __name__ == "__main__":
-    print(f"tamaño de la pantalla es ¨{ANCHO} x {ALTO}")
+    print(f"El tamaño de la pantalla es {ANCHO}x{ALTO}")
     juego = Arkanoid()
     juego.jugar()
