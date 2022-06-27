00b2b24ddad2019898e6b1c1d55e384f6e2c9bbb
Norberto <norbertoalves22@hotmail.com>
2022-06-27 23:30:25 +0200

    falta

--- arkanoid/entidades.py
+++ arkanoid/entidades.py
@@ -3,7 +3,7 @@ import os
 import pygame as pg
 from pygame.sprite import Sprite
 
-from . import ALTO, ANCHO, FPS
+from . import ALTO, ANCHO, COLOR_MENSAJE, FPS
 
 """
 1. Crear una clase Raqueta
@@ -21,13 +21,13 @@ Para animar las imagenes:
 
 class Raqueta(Sprite):
 
-    margen_inferior = 20
+    margen_inferior = 50
     velocidad = 5
     fps_animacion = 12
     limite_iteracion = FPS // fps_animacion
     iteracion = 0
     
-    def __init__(self) -> None:
+    def __init__(self):
         super().__init__()
 
         self.sprites = []
@@ -40,7 +40,8 @@ class Raqueta(Sprite):
         
         self.siguiente_imagen = 0
         self.image = self.sprites[self.siguiente_imagen]
-        self.rect = self.image.get_rect(midbottom=(ANCHO/2, ALTO-self.margen_inferior))
+        self.rect = self.image.get_rect(
+            midbottom=(ANCHO/2, ALTO-self.margen_inferior))
 
     def update(self):
         # Comprbamos si hay tecla pulsada y actualizamos la posición
@@ -77,14 +78,17 @@ class Raqueta(Sprite):
     y queden pegados pero no amontonados
     (juntos pero no revueltos)
 """
+
+
 class Ladrillo(Sprite):
-    def __init__(self, fila, columna):
+    def __init__(self, fila, columna, puntos):
         super().__init__()
 
         ladrillo_verde = os.path.join("resources", "images", "greenTile.png")
         self.image = pg.image.load(ladrillo_verde)
         ancho = self.image.get_width()
         alto = self.image.get_height()
+        self.puntos = puntos
 
         self.rect = self.image.get_rect(x=columna * ancho, y=fila* alto)
 
@@ -93,6 +97,7 @@ class Pelota(Sprite):
 
     velocidad_x = -5
     velocidad_y = -5
+    he_perdido = False
 
     def __init__(self, **kwargs):
         super().__init__()
@@ -114,16 +119,46 @@ class Pelota(Sprite):
                 self.velocidad_y = -self.velocidad_y
 
             if self.rect.top > ALTO:
-                self.pierdes()
-                self.reset()
-
-    def pierdes(self):
-        print("Pierdes una vida")
-
-    def reset(self):
-        print("Recolocamos la pelota en su posición inicial")
+                self.he_perdido = True
 
     def hay_colision(self, otro):
         if self.rect.colliderect(otro):
             # hay colisión
             self.velocidad_y = -self.velocidad_y
+
+
+class ContadorVidas():
+    def __init__(self, vidas_iniciales):
+        self.vidas = vidas_iniciales
+
+    def perder_vida(self):
+        self.vidas -= 1
+        print(f"Has perdido una vida. Te quedan {self.vidas}")
+        return self.vidas < 1
+
+    def pintar(self):
+        """
+        Pendiente de implementar
+        """
+    pass
+
+
+class Marcador():
+    def __init__(self):
+        self.valor = 0
+        font_file = os.path.join(
+            "resources", "fonts", "LibreFranklin-VariableFont_wght.ttf")
+        self.tipografia = pg.font.Font(font_file, 20)
+
+    def aumentar(self, puntos):
+        self.valor += puntos
+
+    def pintar(self, pantalla):
+        """
+        Mostrar el marcador en la línea inferior de la pantalla
+        """
+    mensaje = f"Marcador: {self.valor}"
+    texto = self.tipografia.render(mensaje, True, COLOR_MENSAJE)
+    pos_x = 20
+    pos_y = ALTO-texto.get_height()-10
+    pantalla.blit(texto, (pos_x, pos_y))
